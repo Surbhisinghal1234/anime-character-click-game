66f0019e19ab8d1e5b5c8cfe2332eeb8efff4c7c
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-01-11 15:49:44 +0530

    second commit without media query

--- game.js
+++ game.js
@@ -1,17 +1,101 @@
-const selectCharacter = document.querySelector(".imgbox img");
-const selectGame = document.querySelector("#btn");
-const selectedAnime = document.querySelector("h3");
+const selectCharacter = document.querySelectorAll(".imgbox img");
+const startGame = document.querySelector("#btn");
+
+// const selectedAnime = document.querySelector("h3");
+
 const timeDiv = document.querySelector("#left span");
 
 const scoreDiv = document.querySelector("#right span");
 
 const selectionScreen = document.querySelector("#top");
 
-const playingScreen = document.querySelector("second-page");
+const playingScreen = document.querySelector("#second-page");
 
-selectCharacter.addEventListener("click", () => {
-  //  top.children[1].children[2].classList.add(".selectCharacterImg")
-  console.log("hii");
+const scoreScreen= document.querySelector("#third-page");
 
-  selectedAnime.style.visiblity="hidden";
+const blankBoxImg= document.querySelector("#blank-box-img");
+
+const finalScoreDiv= document.querySelector("#third-page h3 span")
+
+
+let selectedAnime="";
+let timer=20;
+let score=0;
+
+selectCharacter.forEach((char)=>{
+  char.addEventListener("click",(e)=>{
+    console.log(e)   
+    const imgboxes=document.querySelectorAll(".imgbox");
+    imgboxes.forEach((box)=>{
+      box.classList.remove("selectCharacterImg");
+    });
+
+    e.target.parentElement.classList.add("selectCharacterImg");
+
+    const charName= e.target.parentElement.nextElementSibling.innerHTML;
+    document.querySelector("#selectedSuperhero span").innerHTML=charName;
+    selectedAnime = e.target.src;
+  });
 });
+
+startGame.addEventListener("click",()=>{
+  // selectionScreen.Style.display = "none";
+  selectionScreen.style.display="none"
+  // playingScreen.Style.display="flex";
+  playingScreen.style.display="flex"
+
+
+  //console.log(selectedAnime)
+
+  const interval=setInterval(()=>{
+    if (timer===1){
+      clearInterval(interval);
+      playingScreen.style.display="none";
+      scoreScreen.style.display="flex";
+      calculateScore();
+    }
+    timeDiv.innerHTML= --timer;
+  },1000);
+
+  setInterval(()=>{
+    const img=document.createElement("img");
+    img.src=selectedAnime;
+    img.classList.add("smallImg");
+
+    // attach clicking mechanism
+
+    img.addEventListener("click", removeImage);
+
+    // give the image a random position 
+
+    randomPosition(img);
+    blankBoxImg.append(img);
+  },450);
+}
+
+);
+
+// getBoundingClientRect()
+
+function randomPosition(elem){
+  const y= blankBoxImg.clientHeight;
+  const x= blankBoxImg.clientWidth;
+  // console.log(y,x);
+
+  const randomY=Math.random()*y;
+  const randomX=Math.random()*x;
+  elem.style.top=randomY+"px";
+  elem.style.left= randomX+"px";
+
+}
+
+function removeImage(event){
+  event.target.remove();
+  scoreDiv.innerHTML= ++score;
+}
+function calculateScore(){
+  finalScoreDiv.innerHTML=score;
+}
+
+
+
